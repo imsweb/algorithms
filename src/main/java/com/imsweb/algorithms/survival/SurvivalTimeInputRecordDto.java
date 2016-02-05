2b2a9daea4fb6dc1523392b5163eed4e221a4d47
bekeles <bekeles@imsweb.com>
2016-02-05 11:20:15 -0500

    Survival time changes (#359)
    -fixed inspection warning

--- src/main/java/com/imsweb/algorithms/survival/SurvivalTimeInputRecordDto.java
+++ src/main/java/com/imsweb/algorithms/survival/SurvivalTimeInputRecordDto.java
@@ -184,4 +184,14 @@ public class SurvivalTimeInputRecordDto implements Comparable<SurvivalTimeInputR
             }
         }
     }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
