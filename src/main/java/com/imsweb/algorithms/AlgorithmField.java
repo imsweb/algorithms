3ae14b102e91061750fa42cb94b54973e0a333e1
depryf <depryf@imsweb.com>
2019-12-05 12:23:50 -0500

    Started to add more information for the algorithm names (#85)

--- src/main/java/com/imsweb/algorithms/AlgorithmField.java
+++ src/main/java/com/imsweb/algorithms/AlgorithmField.java
@@ -8,22 +8,45 @@ package com.imsweb.algorithms;
  */
 public class AlgorithmField {
 
+    public static String DATA_LEVEL_PATIENT = "Patient";
+    public static String DATA_LEVEL_TUMOR = "Tumor";
+
     public static AlgorithmField of(String id, Integer number, Integer length) {
+        return of(id, number, length, null, null, DATA_LEVEL_TUMOR);
+    }
+
+    public static AlgorithmField of(String id, Integer number, Integer length, String name, String shortName) {
+        return of(id, number, length, name, shortName, DATA_LEVEL_TUMOR);
+    }
+
+    public static AlgorithmField of(String id, Integer number, Integer length, String name, String shortName, String dataLevel) {
         AlgorithmField field = new AlgorithmField();
         field.setId(id);
         field.setNumber(number);
         field.setLength(length);
+        field.setName(name);
+        field.setShortName(shortName);
+        field.setDataLevel(dataLevel);
         return field;
     }
 
     // field ID (for standard fields, it should be the NAACCR XML ID)
     private String _id;
 
-    // NAACCR Number (this will be available only for standard fields)
-    private Integer _number;
+    // field name (for standard fields, it should be the NAACCR item name)
+    private String _name;
+
+    // field short name
+    private String _shortName;
 
     // field length
     private Integer _length;
+
+    // NAACCR Number (this will be available only for standard fields)
+    private Integer _number;
+
+    // data level (corresponds to the NAACCR XML parent tags, usually Patient or Tumor)
+    private String _dataLevel;
 
     public String getId() {
         return _id;
@@ -31,6 +54,30 @@ public class AlgorithmField {
 
     public void setId(String id) {
         _id = id;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
+
+    public String getShortName() {
+        return _shortName;
+    }
+
+    public void setShortName(String shortName) {
+        _shortName = shortName;
+    }
+
+    public Integer getLength() {
+        return _length;
+    }
+
+    public void setLength(Integer length) {
+        _length = length;
     }
 
     public Integer getNumber() {
@@ -41,11 +88,11 @@ public class AlgorithmField {
         _number = number;
     }
 
-    public Integer getLength() {
-        return _length;
+    public String getDataLevel() {
+        return _dataLevel;
     }
 
-    public void setLength(Integer length) {
-        _length = length;
+    public void setDataLevel(String dataLevel) {
+        _dataLevel = dataLevel;
     }
 }
