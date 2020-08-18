e4e12fac7cf7da1e3ef2a50de50c0e6406393c07
howew <howew@imsweb.com>
2020-08-18 09:41:17 -0400

    #91 - implementation of the yost/acs poverty linkage algorithm

--- src/main/java/com/imsweb/algorithms/internal/CensusData.java
+++ src/main/java/com/imsweb/algorithms/internal/CensusData.java
@@ -54,101 +54,197 @@ public class CensusData {
     private String _acsPctPov1418White;
     private String _acsPctPov1418WhiteNonHisp;
 
-    public String getAcsPctPov0610AIAN() { return _acsPctPov0610AIAN; }
+    public String getAcsPctPov0610AIAN() {
+        return _acsPctPov0610AIAN;
+    }
 
-    public void setAcsPctPov0610AIAN(String str) { _acsPctPov0610AIAN = str; }
+    public void setAcsPctPov0610AIAN(String str) {
+        _acsPctPov0610AIAN = str;
+    }
 
-    public String getAcsPctPov0610AllRaces() { return _acsPctPov0610AllRaces; }
+    public String getAcsPctPov0610AllRaces() {
+        return _acsPctPov0610AllRaces;
+    }
 
-    public void setAcsPctPov0610AllRaces(String str) { _acsPctPov0610AllRaces = str; }
+    public void setAcsPctPov0610AllRaces(String str) {
+        _acsPctPov0610AllRaces = str;
+    }
 
-    public String getAcsPctPov0610AsianNHOPI() { return _acsPctPov0610AsianNHOPI; }
+    public String getAcsPctPov0610AsianNHOPI() {
+        return _acsPctPov0610AsianNHOPI;
+    }
 
-    public void setAcsPctPov0610AsianNHOPI(String str) { _acsPctPov0610AsianNHOPI = str; }
+    public void setAcsPctPov0610AsianNHOPI(String str) {
+        _acsPctPov0610AsianNHOPI = str;
+    }
 
-    public String getAcsPctPov0610Black() { return _acsPctPov0610Black; }
+    public String getAcsPctPov0610Black() {
+        return _acsPctPov0610Black;
+    }
 
-    public void setAcsPctPov0610Black(String str) { _acsPctPov0610Black = str; }
+    public void setAcsPctPov0610Black(String str) {
+        _acsPctPov0610Black = str;
+    }
 
-    public String getAcsPctPov0610Hispanic() { return _acsPctPov0610Hispanic; }
+    public String getAcsPctPov0610Hispanic() {
+        return _acsPctPov0610Hispanic;
+    }
 
-    public void setAcsPctPov0610Hispanic(String str) { _acsPctPov0610Hispanic = str; }
+    public void setAcsPctPov0610Hispanic(String str) {
+        _acsPctPov0610Hispanic = str;
+    }
 
-    public String getAcsPctPov0610OtherMulti() { return _acsPctPov0610OtherMulti; }
+    public String getAcsPctPov0610OtherMulti() {
+        return _acsPctPov0610OtherMulti;
+    }
 
-    public void setAcsPctPov0610OtherMulti(String str) { _acsPctPov0610OtherMulti = str; }
+    public void setAcsPctPov0610OtherMulti(String str) {
+        _acsPctPov0610OtherMulti = str;
+    }
 
-    public String getAcsPctPov0610White() { return _acsPctPov0610White; }
+    public String getAcsPctPov0610White() {
+        return _acsPctPov0610White;
+    }
 
-    public void setAcsPctPov0610White(String str) { _acsPctPov0610White = str; }
+    public void setAcsPctPov0610White(String str) {
+        _acsPctPov0610White = str;
+    }
 
-    public String getAcsPctPov0610WhiteNonHisp() { return _acsPctPov0610WhiteNonHisp; }
+    public String getAcsPctPov0610WhiteNonHisp() {
+        return _acsPctPov0610WhiteNonHisp;
+    }
 
-    public void setAcsPctPov0610WhiteNonHisp(String str) { _acsPctPov0610WhiteNonHisp = str; }
+    public void setAcsPctPov0610WhiteNonHisp(String str) {
+        _acsPctPov0610WhiteNonHisp = str;
+    }
 
-    public String getAcsPctPov1014AIAN() { return _acsPctPov1014AIAN; }
+    public String getAcsPctPov1014AIAN() {
+        return _acsPctPov1014AIAN;
+    }
 
-    public void setAcsPctPov1014AIAN(String str) { _acsPctPov1014AIAN = str; }
+    public void setAcsPctPov1014AIAN(String str) {
+        _acsPctPov1014AIAN = str;
+    }
 
-    public String getAcsPctPov1014AllRaces() { return _acsPctPov1014AllRaces; }
+    public String getAcsPctPov1014AllRaces() {
+        return _acsPctPov1014AllRaces;
+    }
 
-    public void setAcsPctPov1014AllRaces(String str) { _acsPctPov1014AllRaces = str; }
+    public void setAcsPctPov1014AllRaces(String str) {
+        _acsPctPov1014AllRaces = str;
+    }
 
-    public String getAcsPctPov1014AsianNHOPI() { return _acsPctPov1014AsianNHOPI; }
+    public String getAcsPctPov1014AsianNHOPI() {
+        return _acsPctPov1014AsianNHOPI;
+    }
 
-    public void setAcsPctPov1014AsianNHOPI(String str) { _acsPctPov1014AsianNHOPI = str; }
+    public void setAcsPctPov1014AsianNHOPI(String str) {
+        _acsPctPov1014AsianNHOPI = str;
+    }
 
-    public String getAcsPctPov1014Black() { return _acsPctPov1014Black; }
+    public String getAcsPctPov1014Black() {
+        return _acsPctPov1014Black;
+    }
 
-    public void setAcsPctPov1014Black(String str) { _acsPctPov1014Black = str; }
+    public void setAcsPctPov1014Black(String str) {
+        _acsPctPov1014Black = str;
+    }
 
-    public String getAcsPctPov1014Hispanic() { return _acsPctPov1014Hispanic; }
+    public String getAcsPctPov1014Hispanic() {
+        return _acsPctPov1014Hispanic;
+    }
 
-    public void setAcsPctPov1014Hispanic(String str) { _acsPctPov1014Hispanic = str; }
+    public void setAcsPctPov1014Hispanic(String str) {
+        _acsPctPov1014Hispanic = str;
+    }
 
-    public String getAcsPctPov1014OtherMulti() { return _acsPctPov1014OtherMulti; }
+    public String getAcsPctPov1014OtherMulti() {
+        return _acsPctPov1014OtherMulti;
+    }
 
-    public void setAcsPctPov1014OtherMulti(String str) { _acsPctPov1014OtherMulti = str; }
+    public void setAcsPctPov1014OtherMulti(String str) {
+        _acsPctPov1014OtherMulti = str;
+    }
 
-    public String getAcsPctPov1014White() { return _acsPctPov1014White; }
+    public String getAcsPctPov1014White() {
+        return _acsPctPov1014White;
+    }
 
-    public void setAcsPctPov1014White(String str) { _acsPctPov1014White = str; }
+    public void setAcsPctPov1014White(String str) {
+        _acsPctPov1014White = str;
+    }
 
-    public String getAcsPctPov1014WhiteNonHisp() { return _acsPctPov1014WhiteNonHisp; }
+    public String getAcsPctPov1014WhiteNonHisp() {
+        return _acsPctPov1014WhiteNonHisp;
+    }
 
-    public void setAcsPctPov1014WhiteNonHisp(String str) { _acsPctPov1014WhiteNonHisp = str; }
+    public void setAcsPctPov1014WhiteNonHisp(String str) {
+        _acsPctPov1014WhiteNonHisp = str;
+    }
 
-    public String getAcsPctPov1418AIAN() { return _acsPctPov1418AIAN; }
+    public String getAcsPctPov1418AIAN() {
+        return _acsPctPov1418AIAN;
+    }
 
-    public void setAcsPctPov1418AIAN(String str) { _acsPctPov1418AIAN = str; }
+    public void setAcsPctPov1418AIAN(String str) {
+        _acsPctPov1418AIAN = str;
+    }
 
-    public String getAcsPctPov1418AllRaces() { return _acsPctPov1418AllRaces; }
+    public String getAcsPctPov1418AllRaces() {
+        return _acsPctPov1418AllRaces;
+    }
 
-    public void setAcsPctPov1418AllRaces(String str) { _acsPctPov1418AllRaces = str; }
+    public void setAcsPctPov1418AllRaces(String str) {
+        _acsPctPov1418AllRaces = str;
+    }
 
-    public String getAcsPctPov1418AsianNHOPI() { return _acsPctPov1418AsianNHOPI; }
+    public String getAcsPctPov1418AsianNHOPI() {
+        return _acsPctPov1418AsianNHOPI;
+    }
 
-    public void setAcsPctPov1418AsianNHOPI(String str) { _acsPctPov1418AsianNHOPI = str; }
+    public void setAcsPctPov1418AsianNHOPI(String str) {
+        _acsPctPov1418AsianNHOPI = str;
+    }
 
-    public String getAcsPctPov1418Black() { return _acsPctPov1418Black; }
+    public String getAcsPctPov1418Black() {
+        return _acsPctPov1418Black;
+    }
 
-    public void setAcsPctPov1418Black(String str) { _acsPctPov1418Black = str; }
+    public void setAcsPctPov1418Black(String str) {
+        _acsPctPov1418Black = str;
+    }
 
-    public String getAcsPctPov1418Hispanic() { return _acsPctPov1418Hispanic; }
+    public String getAcsPctPov1418Hispanic() {
+        return _acsPctPov1418Hispanic;
+    }
 
-    public void setAcsPctPov1418Hispanic(String str) { _acsPctPov1418Hispanic = str; }
+    public void setAcsPctPov1418Hispanic(String str) {
+        _acsPctPov1418Hispanic = str;
+    }
 
-    public String getAcsPctPov1418OtherMulti() { return _acsPctPov1418OtherMulti; }
+    public String getAcsPctPov1418OtherMulti() {
+        return _acsPctPov1418OtherMulti;
+    }
 
-    public void setAcsPctPov1418OtherMulti(String str) { _acsPctPov1418OtherMulti = str; }
+    public void setAcsPctPov1418OtherMulti(String str) {
+        _acsPctPov1418OtherMulti = str;
+    }
 
-    public String getAcsPctPov1418White() { return _acsPctPov1418White; }
+    public String getAcsPctPov1418White() {
+        return _acsPctPov1418White;
+    }
 
-    public void setAcsPctPov1418White(String str) { _acsPctPov1418White = str; }
+    public void setAcsPctPov1418White(String str) {
+        _acsPctPov1418White = str;
+    }
 
-    public String getAcsPctPov1418WhiteNonHisp() { return _acsPctPov1418WhiteNonHisp; }
+    public String getAcsPctPov1418WhiteNonHisp() {
+        return _acsPctPov1418WhiteNonHisp;
+    }
 
-    public void setAcsPctPov1418WhiteNonHisp(String str) { _acsPctPov1418WhiteNonHisp = str; }
+    public void setAcsPctPov1418WhiteNonHisp(String str) {
+        _acsPctPov1418WhiteNonHisp = str;
+    }
 
     public String getIndicatorCode2000() {
         return _indicatorCode2000;
@@ -206,27 +302,51 @@ public class CensusData {
         _povertyIndicators = povertyIndicators;
     }
 
-    public String getYostQuintile0610State() { return _yostQuintile0610State; }
+    public String getYostQuintile0610State() {
+        return _yostQuintile0610State;
+    }
 
-    public void setYostQuintile0610State(String str) { _yostQuintile0610State = str; }
+    public void setYostQuintile0610State(String str) {
+        _yostQuintile0610State = str;
+    }
 
-    public String getYostQuintile0610US() { return _yostQuintile0610US; }
+    public String getYostQuintile0610US() {
+        return _yostQuintile0610US;
+    }
 
-    public void setYostQuintile0610US(String str) { _yostQuintile0610US = str; }
+    public void setYostQuintile0610US(String str) {
+        _yostQuintile0610US = str;
+    }
 
-    public String getYostQuintile1014State() { return _yostQuintile1014State; }
+    public String getYostQuintile1014State() {
+        return _yostQuintile1014State;
+    }
 
-    public void setYostQuintile1014State(String str) { _yostQuintile1014State = str; }
+    public void setYostQuintile1014State(String str) {
+        _yostQuintile1014State = str;
+    }
 
-    public String getYostQuintile1014US() { return _yostQuintile1014US; }
+    public String getYostQuintile1014US() {
+        return _yostQuintile1014US;
+    }
 
-    public void setYostQuintile1014US(String str) { _yostQuintile1014US = str; }
+    public void setYostQuintile1014US(String str) {
+        _yostQuintile1014US = str;
+    }
 
-    public String getYostQuintile1418State() { return _yostQuintile1418State; }
+    public String getYostQuintile1418State() {
+        return _yostQuintile1418State;
+    }
 
-    public void setYostQuintile1418State(String str) { _yostQuintile1418State = str; }
+    public void setYostQuintile1418State(String str) {
+        _yostQuintile1418State = str;
+    }
 
-    public String getYostQuintile1418US() { return _yostQuintile1418US; }
+    public String getYostQuintile1418US() {
+        return _yostQuintile1418US;
+    }
 
-    public void setYostQuintile1418US(String str) { _yostQuintile1418US = str; }
+    public void setYostQuintile1418US(String str) {
+        _yostQuintile1418US = str;
+    }
 }
