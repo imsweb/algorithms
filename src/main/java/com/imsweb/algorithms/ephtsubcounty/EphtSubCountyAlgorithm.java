b0a5b7304aa7994afbc182f4e2944304d7ef3f32
kirbyk <kirbyk@imsweb.com>
2021-08-26 16:25:53 -0400

    Removed unused import (#120)

--- src/main/java/com/imsweb/algorithms/ephtsubcounty/EphtSubCountyAlgorithm.java
+++ src/main/java/com/imsweb/algorithms/ephtsubcounty/EphtSubCountyAlgorithm.java
@@ -5,7 +5,6 @@ package com.imsweb.algorithms.ephtsubcounty;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
