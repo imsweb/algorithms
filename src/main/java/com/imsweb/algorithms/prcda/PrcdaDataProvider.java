308552cbf60610e86993cedc933bc1c3275ee1c6
howew <howew@imsweb.com>
2022-08-12 10:54:30 -0400

    #143 - added linebreak after left curly

--- src/main/java/com/imsweb/algorithms/prcda/PrcdaDataProvider.java
+++ src/main/java/com/imsweb/algorithms/prcda/PrcdaDataProvider.java
@@ -38,7 +38,8 @@ import static com.imsweb.algorithms.prcda.PrcdaUtils.isStateAtDxValid;
  */
 public class PrcdaDataProvider {
 
-    private enum PrcdaYear { YR2017, YR2020;
+    private enum PrcdaYear {
+        YR2017, YR2020;
         public static PrcdaYear getDefault() {
             return YR2020;
         }
