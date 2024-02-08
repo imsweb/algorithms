2a1a3e333b6c27f7d9be48bbb7bd8a911af42cca
depryf <depryf@imsweb.com>
2024-02-08 11:39:30 -0500

    Tweaked derived grade 2018 (#179)

--- src/main/java/com/imsweb/algorithms/derivedgrade/DerivedSummaryGradeUtils.java
+++ src/main/java/com/imsweb/algorithms/derivedgrade/DerivedSummaryGradeUtils.java
@@ -13,9 +13,14 @@ public final class DerivedSummaryGradeUtils {
 
     public static final String UNKNOWN = "9";
 
+    private static final String _BREAST_SCHEMA = "00480";
+
+    private static final String[] _SPECIAL_SCHEMAS = new String[] {"00790", "00795", "00811", "00812", "00821", "00822", "00830"};
+
     private static final String[] _PRIO_BREAST_IN_SITU = new String[] {"H", "M", "L", "3", "2", "1", "D", "C", "B", "A", "9", null};
     private static final String[] _PRIO_BREAST_MALIGNANT = new String[] {"3", "2", "1", "H", "M", "L", "D", "C", "B", "A", "9", null};
-    private static final String[] _PRIO_NON_BREAST = new String[] {"S", "5", "4", "3", "2", "1", "E", "D", "C", "B", "A", "H", "M", "L", "9", null};
+    private static final String[] _PRIO_SPECIAL = new String[] {"8", null};
+    private static final String[] _PRIO_OTHER = new String[] {"S", "5", "4", "3", "2", "1", "E", "D", "C", "B", "A", "H", "M", "L", "9", null};
 
     private DerivedSummaryGradeUtils() {
         // no instances of this class allowed!
@@ -23,14 +28,16 @@ public final class DerivedSummaryGradeUtils {
 
     public static String deriveSummaryGrade(String schemaId, String behavior, String gradeClinical, String gradePathological) {
         String[] priorities = null;
-        if ("00480".equals(schemaId)) {
+        if (_BREAST_SCHEMA.equals(schemaId)) {
             if ("2".equals(behavior))
                 priorities = _PRIO_BREAST_IN_SITU;
             else if ("3".equals(behavior))
                 priorities = _PRIO_BREAST_MALIGNANT;
         }
+        else if (ArrayUtils.contains(_SPECIAL_SCHEMAS, schemaId))
+            priorities = _PRIO_SPECIAL;
         else
-            priorities = _PRIO_NON_BREAST;
+            priorities = _PRIO_OTHER;
 
         if (priorities != null) {
 
